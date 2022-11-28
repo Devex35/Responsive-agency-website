6d16e47ed210cbfeb304c3777214df062e74f79e
Devex35 <exanton904@gmail.com>
2022-11-28 00:44:58 -0800

    comma fix #2

--- components/Contact.js
+++ components/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
         Contact <span className="font-bold">Us</span>{" "}
       </h1>
       <p className=" pt-6 text-xl md:mx-40 mx-10 text-center text-violet-500">
-        Have a great idea? Let's get in touch. The coffee is on us!
+        Have a great idea? Let&apos;s get in touch. The coffee is on us!
       </p>
       <div className="flex justify-center items-center gap-10">
         <div className="group md:w-72 w-80 h-12 rounded-md my-10  cursor-pointer flex justify-center items-center  hover:shadow-lg border border-violet-500 hover:bg-violet-500 ">
